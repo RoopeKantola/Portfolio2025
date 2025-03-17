3313be7eda8c13f3a383b268507b07c92453f386
Roope Kantola <kantolar@student.uef.fi>
2025-03-17 12:14:58 +0200

    lisätty plotly

--- portfolio2025/clustering/testfile.py
+++ portfolio2025/clustering/testfile.py
@@ -1,22 +1,28 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as anim
+import plotly.express as px
+from jinja2 import Template
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-df = pd.DataFrame(np.random.randint(0, 100, size=(100, 2)), columns=["a", "b"])
 
-plt.scatter(df["a"].values.tolist(), df["b"].values.tolist())
-plt.show()
+def draw_graph():
+    df = pd.DataFrame(np.random.randint(0, 100, size=(100, 2)), columns=["a", "b"])
+
+    fig = px.scatter(x=df["a"], y=df["b"])
+
+    output_html_path=r"/graph.html"
+    input_template_path = r"/clustering/index.html"
+
+    plotly_jinja_data = {"fig":fig.to_html(full_html=False)}
+    #consider also defining the include_plotlyjs parameter to point to an external Plotly.js as described above
+
+    with open(output_html_path, "w", encoding="utf-8") as output_file:
+        with open(input_template_path) as template_file:
+            j2_template = Template(template_file.read())
+            output_file.write(j2_template.render(plotly_jinja_data))
+
 
 if __name__ == "__main__":
-    print(df.head())
-
-
-
-
-
-
-
+    pass
--- portfolio2025/clustering/views.py
+++ portfolio2025/clustering/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render
 from main.models import Project
+from .testfile import draw_graph
 
 
 # Create your views here.
 def home(response):
-
     projects = Project.objects.all()
     print(projects)
 
     context = {
         "projects": projects,
-               }
-
+    }
 
     return render(response, "clustering/home.html", context=context)
 
 
 def index(response, id):
-
     project = Project.objects.get(id=id)
+    graph = draw_graph()
     print(project)
 
     context = {
-        "project": project
+        "project": project,
+        "graph": graph,
     }
 
     return render(response, "clustering/index.html", context=context)
