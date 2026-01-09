4a9af5bc921ddf038e8f4b58357b020c9cef6831
Roope Kantola <kantolar@student.uef.fi>
2026-01-09 11:58:12 +0200

    added an allowed host

--- portfolio2025/portfolio2025/settings.py
+++ portfolio2025/portfolio2025/settings.py
@@ -27,9 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6&=!$51ev52((d^7x%54)p%j_wm)5!$qq43mfxby011ni4ydtr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'roopekantola.pythonanywhere.com',
+]
 
 
 # Application definition
